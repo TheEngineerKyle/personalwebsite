72014ea230f112f21fc24700ad9d42e9df638891
TheEngineerKyle <57205263+TheEngineerKyle@users.noreply.github.com>
2023-01-28 17:38:09 -0500

    fasdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,6 +102,21 @@
 					popupCloserText: '',
 					popupLoaderText: '',
 					selector: '.work-item a.image',
+					usePopupCaption: false,
+					usePopupDefaultStyling: false,
+					usePopupEasyClose: false,
+					usePopupNav: true,
+					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+				});
+
+
+				$('#five').poptrox({
+					caption: function($a) { return $a.next('h3').text(); },
+					overlayColor: '#2c2c2c',
+					overlayOpacity: 0.85,
+					popupCloserText: '',
+					popupLoaderText: '',
+					selector: '.work-item a.image',
 					usePopupCaption: true,
 					usePopupDefaultStyling: false,
 					usePopupEasyClose: false,
