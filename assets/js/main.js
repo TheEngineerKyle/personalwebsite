0a15ea6fc7d7d39503f8771edf9239a0698ab254
Kyle Linde <kylelinde@Kyles-Air.fios-router.home>
2024-12-14 15:15:00 -0500

    debug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-(function($) {
+(function ($) {
 
 	var $window = $(window),
 		$body = $('body'),
@@ -9,141 +9,142 @@
 		settings = {
 
 			// Parallax background effect?
-				parallax: true,
+			parallax: true,
 
 			// Parallax factor (lower = more intense, higher = less intense).
-				parallaxFactor: 20
+			parallaxFactor: 20
 
 		};
 
+
 	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1800px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ '481px',   '736px'  ],
-			xsmall:  [ null,      '480px'  ],
-		});
+	breakpoints({
+		xlarge: ['1281px', '1800px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: [null, '480px'],
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Touch?
-		if (browser.mobile) {
+	if (browser.mobile) {
 
-			// Turn on touch mode.
-				$body.addClass('is-touch');
+		// Turn on touch mode.
+		$body.addClass('is-touch');
 
-			// Height fix (mostly for iOS).
-				window.setTimeout(function() {
-					$window.scrollTop($window.scrollTop() + 1);
-				}, 0);
+		// Height fix (mostly for iOS).
+		window.setTimeout(function () {
+			$window.scrollTop($window.scrollTop() + 1);
+		}, 0);
 
-		}
+	}
 
 	// Footer.
-		breakpoints.on('<=medium', function() {
-			$footer.insertAfter($main);
-		});
+	breakpoints.on('<=medium', function () {
+		$footer.insertAfter($main);
+	});
 
-		breakpoints.on('>medium', function() {
-			$footer.appendTo($header);
-		});
+	breakpoints.on('>medium', function () {
+		$footer.appendTo($header);
+	});
 
 	// Header.
 
-		// Parallax background.
+	// Parallax background.
 
-			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-				if (browser.name == 'ie'
-				||	browser.mobile)
-					settings.parallax = false;
+	// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+	if (browser.name == 'ie'
+		|| browser.mobile)
+		settings.parallax = false;
 
-			if (settings.parallax) {
+	if (settings.parallax) {
 
-				breakpoints.on('<=medium', function() {
+		breakpoints.on('<=medium', function () {
 
-					$window.off('scroll.strata_parallax');
-					$header.css('background-position', '');
+			$window.off('scroll.strata_parallax');
+			$header.css('background-position', '');
 
-				});
+		});
 
-				breakpoints.on('>medium', function() {
+		breakpoints.on('>medium', function () {
 
-					$header.css('background-position', 'left 0px');
+			$header.css('background-position', 'left 0px');
 
-					$window.on('scroll.strata_parallax', function() {
-						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-					});
-
-				});
-
-				$window.on('load', function() {
-					$window.triggerHandler('scroll');
-				});
-
-			}
-
-	// Main Sections.
-
-		// Lightbox gallery.
-
-
-
-		
-		$window.on('load', function() {
-
-			$('#two').poptrox({
-				
-				caption: function($a) { return $a.next('h3').text(); },
-				overlayColor: '#2c2c2c',
-				overlayOpacity: 0.85,
-				popupCloserText: '',
-				popupLoaderText: '',
-				selector: '.work-item a.image',
-				usePopupCaption: false,
-				usePopupDefaultStyling: false,
-				usePopupEasyClose: false,
-				usePopupNav: true,
-				windowMargin: (breakpoints.active('<=small') ? 0 : 50)
-			});
-
-			$('#anysis').poptrox({
-				caption: function($a) { return $a.next('h3').text(); },
-				overlayColor: '#2c2c2c',
-				overlayOpacity: 0.85,
-				popupCloserText: '',
-				popupLoaderText: '',
-				selector: '.work-item a.image',
-				usePopupCaption: true,
-				usePopupDefaultStyling: false,
-				usePopupEasyClose: false,
-				usePopupNav: true,
-				windowMargin: (breakpoints.active('<=small') ? 0 : 50)
-			});
-
-			
-			$('#projects').poptrox({
-				caption: function($a) { return $a.next('h3').text(); },
-				overlayColor: '#2c2c2c',
-				overlayOpacity: 0.85,
-				popupCloserText: '',
-				popupLoaderText: '',
-				selector: '.work-item a.image',
-				usePopupCaption: false,
-				usePopupDefaultStyling: false,
-				usePopupEasyClose: false,
-				usePopupNav: true,
-				windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+			$window.on('scroll.strata_parallax', function () {
+				$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
 			});
 
 		});
 
-			
+		$window.on('load', function () {
+			$window.triggerHandler('scroll');
+		});
+
+	}
+
+	// Main Sections.
+
+	// Lightbox gallery.
+
+
+
+
+	$window.on('load', function () {
+
+		$('#two').poptrox({
+
+			caption: function ($a) { return $a.next('h3').text(); },
+			overlayColor: '#2c2c2c',
+			overlayOpacity: 0.85,
+			popupCloserText: '',
+			popupLoaderText: '',
+			selector: '.work-item a.image',
+			usePopupCaption: false,
+			usePopupDefaultStyling: false,
+			usePopupEasyClose: false,
+			usePopupNav: true,
+			windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+		});
+
+		$('#anysis').poptrox({
+			caption: function ($a) { return $a.next('h3').text(); },
+			overlayColor: '#2c2c2c',
+			overlayOpacity: 0.85,
+			popupCloserText: '',
+			popupLoaderText: '',
+			selector: '.work-item a.image',
+			usePopupCaption: true,
+			usePopupDefaultStyling: false,
+			usePopupEasyClose: false,
+			usePopupNav: true,
+			windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+		});
+
+
+		$('#projects').poptrox({
+			caption: function ($a) { return $a.next('h3').text(); },
+			overlayColor: '#2c2c2c',
+			overlayOpacity: 0.85,
+			popupCloserText: '',
+			popupLoaderText: '',
+			selector: '.work-item a.image',
+			usePopupCaption: false,
+			usePopupDefaultStyling: false,
+			usePopupEasyClose: false,
+			usePopupNav: true,
+			windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+		});
+
+	});
+
+
 
 
 
